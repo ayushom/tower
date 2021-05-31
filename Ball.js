93b578456843ca32d91eb3c11fd18e4593d48ee5
ayushom <75958119+ayushom@users.noreply.github.com>
2021-05-31 14:43:12 -0400

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,36 @@
+class Ball
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:1,
+			friction:1,
+			density:1.2
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+		
+
+
+	}
+	display()
+	{
+			var ballpos=this.body.position;		
+			push()
+			translate(ballpos.x, ballpos.y);
+			// rectMode(CENTER)
+
+			// rotate(this.body.angle)
+			fill(255,0,255)
+			imageMode(CENTER);
+			ellipseMode(RADIUS)
+			
+			pop()
+			
+	}
+
+}
